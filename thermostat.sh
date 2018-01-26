bad61662a4cbe6d76c1ed24ada6539f9be421c32
C3DMaX <maxxka07@gmail.com>
2018-01-26 13:21:27 +0100

    add window open mode and usage instructions

--- thermostat.sh
+++ thermostat.sh
@@ -15,8 +15,17 @@ case ${option} in
 			../eq3/eq3.exp 00:1A:22:07:05:AC sync
 			;;
 
+	-wopen)
+			echo "Open window mode for $2 minutes"
+			../eq3/eq3.exp 00:1A:22:07:05:AC window 15 00:$2
+			;;
+
 	*)
 
 			echo "Max thermostat control CLI"
+			echo "Usage:"
+			echo "-temp N.m		Set thermostat to N.m degrees celsius"
+			echo "-status 		Fetch current thermostat settings"
+			echo "-wopen X		Set thermostat to open window mode for X minutes"
 
 esac
