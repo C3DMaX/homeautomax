ea31806980cc9a23e916e7b8f48a992292a0e509
C3DMaX <maxxka07@gmail.com>
2019-01-31 13:46:15 +0100

    Update thermostat.sh

--- thermostat.sh
+++ thermostat.sh
@@ -8,12 +8,12 @@ case ${option} in
 
 	-temp)
 			echo "Setting thermostat to $2 C°"
-			/eq3/eq3.exp 00:1A:22:07:05:AC temp $2
+			./eq3/eq3.exp 00:1A:22:07:05:AC temp $2
 			;;
 
 	-status)
 			echo "Fetching current thermostat settings..."
-			/eq3/eq3.exp 00:1A:22:07:05:AC sync
+			./eq3/eq3.exp 00:1A:22:07:05:AC sync
 			;;
 
 
